fa1f78c89f674a5d4743674bb7b99f034a8fbc8a
melvyniandrag <melvyndrag@yahoo.com>
2019-01-28 09:09:25 -0500

    added return value

--- Week2/miscellaneousBash/functions.sh
+++ Week2/miscellaneousBash/functions.sh
@@ -6,6 +6,13 @@ hello_you(){
     echo "hello $1"
 }
 
+I_have_a_retval(){
+    # no way to return a string. Bash knows about return codes and strings written to stdout, stderr.
+    return 1
+}
+
 hello_world
 hello_you melvyn
 hello_you $(whoami)
+I_have_a_retval
+echo $?
