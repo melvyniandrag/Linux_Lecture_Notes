6a1792f1784bc3893d49d53eaa963e4197ca3d48
melvyniandrag <melvyniandrag@gmail.com>
2019-04-18 14:06:23 -0400

    Create  endianness.cpp

---
+++ Week12/Endianness/endianness.cpp
@@ -0,0 +1,9 @@
+#include <cstdint>
+#include <cstdio>
+
+int main(){
+        uint32_t num = 64;
+        char * numAddr = (char*)&num ;
+        for ( size_t i = 0; i < sizeof(num); ++i )
+                printf("0x%02X\n", (int)numAddr[i]);
+}
