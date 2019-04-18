3ad61080a50212e911adcb511feb65a287810079
melvyniandrag <melvyniandrag@gmail.com>
2019-04-18 14:07:10 -0400

    Update endianness.cpp

--- Week12/Endianness/endianness.cpp
+++ Week12/Endianness/endianness.cpp
@@ -5,5 +5,5 @@ int main(){
         uint32_t num = 64;
         char * numAddr = (char*)&num ;
         for ( size_t i = 0; i < sizeof(num); ++i )
-                printf("0x%02X\n", (int)numAddr[i]);
+                printf("0x%02X\n", numAddr[i]);
 }
