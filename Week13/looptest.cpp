8c322e17593717ec5ffc2dc4311e1280b6c1e8f5
melvyniandrag <melvyniandrag@gmail.com>
2019-04-23 11:25:50 -0400

    Update looptest.cpp

--- Week13/looptest.cpp
+++ Week13/looptest.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cassert>
 
 int main()
 {
+	assert( sizeof(unsigned long long) == 8);
 	const unsigned long long PRINT_FREQ = 1000000000;
 	char c = 0;
 	for ( unsigned long long i = 0; i < UINT64_MAX; ++i )
