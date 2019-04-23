e2cb6dc614a6d95bcbce08cec19c8ee2f09e0499
melvyniandrag <melvyniandrag@gmail.com>
2019-04-23 11:24:25 -0400

    Create looptest.cpp

---
+++ Week13/looptest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	const unsigned long long PRINT_FREQ = 1000000000;
+	char c = 0;
+	for ( unsigned long long i = 0; i < UINT64_MAX; ++i )
+	{
+		for ( unsigned long long j = 0; j < UINT64_MAX; ++j )
+		{
+			if( j % PRINT_FREQ == 0 )
+			       	std::cout << i << "/" << UINT64_MAX << " , "<< j << "/" << UINT64_MAX << std::endl;
+			c++;
+		}  
+	}
+	std::cout << c << std::endl;
+}
